2f050cf916c492cba8419865a8727fd2f4582f64
Tejaswee <handge.tejaswee@gmail.com>
2026-01-26 17:31:14 +0530

    deletion added

--- linkedList/deletion.py
+++ linkedList/deletion.py
@@ -26,7 +26,6 @@ def delete_end(head:Node):
     temp.next=None
     return head
 
-
 # delete element at a position of linked list
 def delete_the_pos(head:Node, pos:int):
     if head==None:
@@ -54,7 +53,6 @@ def delete_the_pos(head:Node, pos:int):
         temp=temp.next
         c+=1
     return None
-
                        
 # delete the value in linked list
 def delete_the_val(head:Node, val:int):
@@ -81,35 +79,48 @@ def delete_the_val(head:Node, val:int):
         temp=temp.next
         prev=prev.next
     return None
-# delete element before the value in linked list---⌛⌛
+# delete element before the value in linked list
 def delete_ele_b4_value(head:Node, val: int):
     if head==None :
         return None
     if head.data==val or head.next==None:
         return head
     if head.next.data==val:
-        # print(head.next.data)
         temp=head
         head=head.next
         temp.next=None
         return head
+
     prev=head
     temp=head.next
     while(temp!=None):
-        if temp.data==val:
-            if temp.next==None:
-                prev.next=None
-                return head
-            else:
-                prev.next==temp.next
+        if temp.next!=None:
+            if temp.next.data==val:
+                prev.next=temp.next
+                temp.next=None
                 return head
         prev=prev.next
         temp=temp.next
     return None
-
-
 # delete element after the value in linked list
-
+def delete_ele_af_val(head:Node,val:int):
+    if head==None or head.next==None:
+        return head
+    temp=head
+    while(temp!=None):
+        if temp.data==val:
+            if temp.next==None:
+                return head
+            if temp.next.next!=None:
+                new=temp.next
+                temp.next=temp.next.next
+                new.next=None
+                return head
+            else:
+                temp.next=None
+                return head
+        temp=temp.next
+    return head
 if __name__=="__main__":
     # traversal_ll(delete_head(head))
     # traversal_ll(delete_end(head))
@@ -117,4 +128,4 @@ if __name__=="__main__":
     # traversal_ll(delete_ele_pos(c,2))
     # traversal_ll(delete_the_val(c,2))
     # traversal_ll(delete_the_pos(head,3))
-    traversal_ll(delete_ele_b4_value(head,3))
+    traversal_ll(delete_ele_af_val(head,5))
