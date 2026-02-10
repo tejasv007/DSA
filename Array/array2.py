26fa4e4e16306867f2058fdee9552fa6c38a69d1
Tejaswee <handge.tejaswee@gmail.com>
2026-02-10 16:32:11 +0530

    array updated

--- Array/array2.py
+++ Array/array2.py
@@ -3,3 +3,24 @@
 Docstring for Array.array1
     to solve a question firstly use brute force approach --> better--> optimal
 '''
+# Left rotate the array by one place
+# brute force---TC-O(N)--optimal also
+def leftRotateOne(a:list,n:int):
+    new=a[0]
+    a.remove(a[0])
+    a.append(new)
+    return a
+
+# Left rotate the array by k place
+# brute force---TC=O()---left
+def leftRotateK(a:list, n:int,k:int):
+    if n==k:
+        return a
+    new1=a[:k]
+    new2=a[k:]
+    new2.extend(new1)
+    return new2
+
+if __name__=="__main__":
+    a=[1,2,3,4,5]
+    print(leftRotateK(a,5,4))
